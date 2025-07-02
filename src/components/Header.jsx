c4b6df43219dd9aaae499cfca977c586a77c5b76
AvulaRamSwaroop <avulamuralikrishna5@gmail.com>
2025-07-02 14:30:58 +0530

    Header.jsx modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,6 @@ const Header = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
             <span className="text-2xl font-bold text-orange-400">vendora</span>
-            {/* <span className="text-sm">.clone</span> */}
           </Link>
 
           {/* Search Bar */}
